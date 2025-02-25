ae33bf83376d502d61f0de8b41a1c0749b66931e
Ishrat Jahan Mohima <ishratmohima@Ishrats-MacBook-Pro.local>
2025-02-24 23:41:18 -0500

    Initial Commit - My Classmates App

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -6,15 +6,15 @@ const PersonCard = ({ person }) => {
   const [likes, setLikes] = useState(0); // useState for tracking the number of likes
 
   return (
-    <Card  className="shadow-sm border-0 rounded p-8 my-1"   // Bootstrap classes for styling card
-    style={{ backgroundColor: "#E5E5E5", width: "40rem", height: "9rem",  borderRadius: "12px"  }} // Customized size and background color of card
+    <Card  className="shadow-sm rounded p-8 my-1"   // Bootstrap classes for styling card
+    
     >
       <Card.Body>
         <Card.Text>Name: {person.name}</Card.Text>
         <Card.Text>Favorite Food: {person.favoriteFood}</Card.Text>
         <div className="d-flex justify-content-between align-items-center">
           <Card.Text>Favorite Color: {person.favoriteColor}</Card.Text>
-          <Button onClick={() => setLikes(likes + 1)} variant="dark">
+          <Button onClick={() => setLikes(likes + 1)} variant="primary">
             Like ({likes})
           </Button>
         </div>
